60a03a0480c9f6e65afd6bc3eb03a7fdab74f0de
CodinaFJ <fjavier.codina@gmail.com>
2023-07-30 19:26:02 +0200

    ending music

---
+++ Assets/Scripts/Level/FinalCinematic.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalCinematic : MonoBehaviour
+{
+
+    void Start()
+    {
+        AudioManager.instance.PlayMusic("Ending");
+    }
+}
